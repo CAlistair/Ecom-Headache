8d24de826b61cd9cc250807f01f68173f8049a58
CAlistair <128975514+CAlistair@users.noreply.github.com>
2023-10-10 11:21:27 +0100

    Changes in details and colour

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft  } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faShoppingCart, faTimes, faBolt, faDesktop, faTags, faStar, faStarHalfAlt, faArrowLeft  } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft );
+library.add(faBars, faShoppingCart, faTimes, faBolt, faDesktop, faTags, faStar, faStarHalfAlt, faArrowLeft );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
