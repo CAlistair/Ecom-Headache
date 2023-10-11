53f75654bbc571449ba62ba9ee7456644f539907
CAlistair <128975514+CAlistair@users.noreply.github.com>
2023-10-11 15:43:10 +0100

    Bug fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,16 +7,15 @@ const Footer = () => {
         <footer>
             <div className="container">
                 <div className="row row__column">
-                    <Link to="">
+                    <a href="#">
                         <figure className="footer__logo">
                             <img src={Logo} className="footer__logo--img" alt="" />
                         </figure>
-                    </Link>
+                    </a>
                     <div className="footer__list">
                         <Link to="/" className="footer__link">Home</Link>
                         <span className="footer__link no-cursor">About</span>
-                        <Link to="/books" className="footer__link">Books</Link>
-                        <Link to="/books" className="footer__link">Books</Link>
+                        <Link to="/books" className="footer__link">Monitors</Link>
                         <Link to="/cart" className="footer__link">Cart</Link>
                     </div>
                     <div className="footer__copyright">Copyright &copy; 2023 View</div>
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
             <header>
                 <div className="header__container">
                     <div className="header__description">
-                        <h1><span className="blue"> UK's leading monitor distributors</span></h1>
+                        <h1><span className="blue"> Best monitor distrubutor worldwide</span></h1>
                         <h2>Find your dream monitor with <span className="blue"> View</span></h2>
                         <a href="#features">
                             <button className="btn">Browse Monitors</button>
